b497903b0764099c29ccae6d9c8eafdc30164808
Floran Pagliai <floran.pagliai@gmail.com>
2013-05-23 20:39:34 +0200

    Gestion du déplacement en fonction de la vitesse

--- src/Player.cpp
+++ src/Player.cpp
@@ -21,7 +21,8 @@ namespace Model
     /// Charge le modele
     this->model_ = gdl::Model::load("assets/marvin.fbx");
     model_.set_default_model_color(gdl::Color(255,0,0));
-    //model_.set_anim_speed("Take 001", 1); //pour changer la vitesse quand il court
+    speed_ = 4;
+    model_.set_anim_speed("Take 001", 1 + this->speed_); //pour changer la vitesse quand il court
     //model_.infos(); // pour avoir les infos sur le personnage
     model_.cut_animation(this->model_, "Take 001", 40, 50, "Run");
     model_.cut_animation(this->model_, "Take 001", 0, 0, "Stop");
@@ -29,48 +30,41 @@ namespace Model
 
   void Player::update(gdl::GameClock const & gameClock, gdl::Input & input)
   {
-    /// Si une animation est en cours, nous la mettons a jour
     this->model_.update(gameClock);
-    /*model_.set_default_model_color(gdl::Color(255,0,0));
-    //model_.set_anim_speed("Take 001", 1); //pour changer la vitesse quand il court
-    //model_.infos(); // pour avoir les infos sur le personnage
-    model_.cut_animation(this->model_, "Take 001", 40, 50, "Run");
-    model_.cut_animation(this->model_, "Take 001", 0, 0, "Stop");
-    */
     if (input.isKeyDown(gdl::Keys::Up) == true)
       {
         this->rotation_.y = 180;
-        this->position_.y += 45.0f;
+        this->position_.y += 40.0f + speed_ * 5.0f;
 	this->model_.play("Run");
       }
     else if (input.isKeyDown(gdl::Keys::Down) == true)
       {
         this->rotation_.y = 0;
-        this->position_.y -= 45.0f;
+        this->position_.y -= 40.0f + speed_ * 5.0f;
         this->model_.play("Run");
       }
     else if (input.isKeyDown(gdl::Keys::Left) == true)
     {
       this->rotation_.y = -90;
-      this->position_.x -= 45.0f;
+      this->position_.x -= 40.0f + speed_ * 5.0f;
       this->model_.play("Run");
     }
     else if (input.isKeyDown(gdl::Keys::Right) == true)
       {
         this->rotation_.y = 90;
-        this->position_.x += 45.0f;
+        this->position_.x += 40.0f + speed_ * 5.0f;
 	this->model_.play("Run");
       }
     else if (input.isKeyDown(gdl::Keys::Escape) == true)
       {
 	exit (EXIT_FAILURE);
       }
-    else if (input.isKeyDown(gdl::Keys::B) == true)
+    else
+        this->model_.play("Stop");
+    if (input.isKeyDown(gdl::Keys::B) == true)
       {
 	this->objects_->push_front(new Bomb::Bombe(this->position_.x, this->position_.y, this->objects_));
       }
-    else
-        this->model_.play("Stop");
   }
 
   void	Player::draw(void)
