8daee4b6d0cc029fc1488789f231ca4fb71c21a2
Floran Pagliai <floran.pagliai@gmail.com>
2013-06-07 01:18:35 +0200

    Résolution beug pour sortir de la bombe placé

--- src/Bomberman.cpp
+++ src/Bomberman.cpp
@@ -61,10 +61,10 @@ void Bomberman::initialize(void) {
     model_.cut_animation(this->model_, "Take 001", 80, 120, "EndRun");
     model_.cut_animation(this->model_, "Take 001", 0, 0, "Stop");
     if (this->id_ == 0)
-      model_.set_default_model_color(gdl::Color(255, 0, 0));
+        model_.set_default_model_color(gdl::Color(255, 0, 0));
     else
         model_.set_default_model_color(gdl::Color(0, 0, 255));
-    
+
     powerupSound_ = new sf::Music();
     deathSound_ = new sf::Music();
     powerupSound_->OpenFromFile("assets/sound/pop.wav");
@@ -96,8 +96,14 @@ bool Bomberman::checkMove(gdl::Input &input, float distX, float distZ) {
     for (it = this->objects_->begin(); it != this->objects_->end(); ++it) {
         if ((this->checkCollision((*it)->getPosition().x - distX, (*it)->getPosition().z) || this->checkCollision((*it)->getPosition().x, (*it)->getPosition().z - distZ))
                 && ((*it)->getType() == WALL || (*it)->getType() == CRATE || (*it)->getType() == BOMB)) {
-            if ((this->checkCollision((*it)->getPosition().x, (*it)->getPosition().z)) && ((*it)->getType() == BOMB))
+            if ((this->checkCollision((*it)->getPosition().x, (*it)->getPosition().z)) && ((*it)->getType() == BOMB)) {
+                for (std::list<AObject*>::iterator it2 = this->objects_->begin(); it2 != this->objects_->end(); ++it2) {
+                    if ((this->checkCollision((*it2)->getPosition().x - distX, (*it2)->getPosition().z) || this->checkCollision((*it2)->getPosition().x, (*it2)->getPosition().z - distZ))
+                            && ((*it2)->getType() == WALL || (*it2)->getType() == CRATE))
+                        return false;
+                }
                 return true;
+            }
             return false;
         } else if (this->checkCollision((*it)->getPosition().x, (*it)->getPosition().z) && (*it)->getType() == BONUS && this->getBonus(dynamic_cast<Bonus *> (*it))) {
             delete (*it);
@@ -177,11 +183,11 @@ int Bomberman::getSpeed() const {
 }
 
 int Bomberman::getId() const {
-  return id_;
+    return id_;
 }
 
 int Bomberman::getAmmoStock() const {
-  return ammoStock_;
+    return ammoStock_;
 }
 
 bool Bomberman::ammoUp() {
